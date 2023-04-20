1f263313c0693f5fb04b73acbee23e453356e4ed
Guilherme010101 <126472740+Guilherme010101@users.noreply.github.com>
2023-04-20 16:00:45 +0100

    Add files via upload

---
+++ 1_Tutorials/1-2_Arduino_IDE_Tutorials/Tutorial_3_-_Follow_Path/Tutorial_3_exemplo_2/EDURobot_tutorial_3_exemplo_2/EDURobot_tutorial_3_exemplo_2.ino
@@ -0,0 +1,100 @@
+#include <iModBot.h>
+iModBot robot;
+
+void setup()
+ 
+{
+  robot.begin();
+  Serial.begin(115200);
+}
+
+void loop() 
+
+{
+  if ((robot.readS1() == 1) && (robot.readS2() == 1) && (robot.readS3() == 0) && (robot.readS4() == 1) && (robot.readS5() == 1))   
+  { 
+    robot.forward(110);
+    while ((robot.readS1() == 1) && (robot.readS2() == 1) && (robot.readS3() == 0) && (robot.readS4() == 1) && (robot.readS5() == 1))
+    {
+      ;
+    }
+  }
+  
+  else if ((robot.readS1() == 1) && (robot.readS2() == 0) && (robot.readS3() == 1) && (robot.readS4() == 1) && (robot.readS5() == 1))
+  {
+    robot.rotateLeft(110);
+    while ((robot.readS1() == 1) && (robot.readS2() == 0) && (robot.readS3() == 1) && (robot.readS4() == 1) && (robot.readS5() == 1))
+    {
+      ;
+    } 
+  }
+  
+  else if ((robot.readS1() == 0) && (robot.readS2() == 1) && (robot.readS3() == 1) && (robot.readS4() == 1) && (robot.readS5() == 1))
+  {+
+    robot.rotateLeft(110);
+    while ((robot.readS1() == 0) && (robot.readS2() == 1) && (robot.readS3() == 1) && (robot.readS4() == 1) && (robot.readS5() == 1))
+    {
+      ;
+    }
+  }
+  
+  else if ((robot.readS1() == 1) && (robot.readS2() == 1) && (robot.readS3() == 1) && (robot.readS4() == 0) && (robot.readS5() == 1))
+  {
+    robot.rotateRight(110);
+    while ((robot.readS1() == 1) && (robot.readS2() == 1) && (robot.readS3() == 1) && (robot.readS4() == 0) && (robot.readS5() == 1))
+    {
+      ;
+    }
+  }
+  
+  else if ((robot.readS1() == 1) && (robot.readS2() == 1) && (robot.readS3() == 1) && (robot.readS4() == 1) && (robot.readS5() == 0))
+  {
+    robot.rotateRight(110);
+    while ((robot.readS1() == 1) && (robot.readS2() == 1) && (robot.readS3() == 1) && (robot.readS4() == 1) && (robot.readS5() == 0))
+    {
+      ;
+    }
+  }
+  
+  else if ((robot.readS1() == 1) && (robot.readS2() == 0) && (robot.readS3() == 0) && (robot.readS4() == 1) && (robot.readS5() == 1))
+  {
+    robot.rotateLeft(110);
+    while ((robot.readS1() == 1) && (robot.readS2() == 0) && (robot.readS3() == 0) && (robot.readS4() == 1) && (robot.readS5() == 1))
+    {
+      ;
+    }
+  }
+  
+  else if ((robot.readS1() == 1) && (robot.readS2() == 1) && (robot.readS3() == 0) && (robot.readS4() == 0) && (robot.readS5() == 1))
+  {
+    robot.rotateRight(110);
+    while ((robot.readS1() == 1) && (robot.readS2() == 1) && (robot.readS3() == 0) && (robot.readS4() == 0) && (robot.readS5() == 1))
+    {
+      ;
+    }
+  }
+  
+  else if ((robot.readS1() == 1) && (robot.readS2() == 1) && (robot.readS3() == 0) && (robot.readS4() == 0) && (robot.readS5() == 0))
+  {
+    robot.rotateRight(110);
+    while ((robot.readS1() == 1) && (robot.readS2() == 1) && (robot.readS3() == 0) && (robot.readS4() == 0) && (robot.readS5() == 0))
+    {
+      ;
+    }
+  }
+
+  else if ((robot.readS1() == 0) && (robot.readS2() == 0) && (robot.readS3() == 0) && (robot.readS4() == 1) && (robot.readS5() == 1))
+  {
+    robot.rotateLeft(110);
+    while ((robot.readS1() == 0) && (robot.readS2() == 0) && (robot.readS3() == 0) && (robot.readS4() == 1) && (robot.readS5() == 1))
+    {
+      ;
+    }
+  }
+  
+  else if (((robot.readS1() == 0) && (robot.readS2() == 0) && (robot.readS3() == 0) && (robot.readS4() == 0) && (robot.readS5() == 0)) || (robot.readCLP() == 1) || (robot.distance() <= 10))
+  {
+      robot.stopMotors();
+  }
+
+}
